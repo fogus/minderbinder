d2eb0c67e6398d55581483cc9c117a18f341706c
Fogus <mefogus@gmail.com>
2013-04-07 09:46:11 -0300

    Upgrade Clojure version to v1.5.1

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject fogus/minderbinder "0.3.0-SNAPSHOT"
   :description "Converting one thing into another thing via Clojure at read time and compile time."
-  :dependencies [[org.clojure/clojure "1.5.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :plugins [[lein-swank "1.4.4"]
             [marginalia "0.8.0-SNAPSHOT"]])
