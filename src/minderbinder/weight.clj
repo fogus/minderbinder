66fef7807940c45bf35fda8fe5b6f3c2db618da5
fogus <mefogus@gmail.com>
2013-06-04 08:21:24 -0400

    Maintain consistency in use of plurals

--- src/minderbinder/weight.clj
+++ src/minderbinder/weight.clj
@@ -3,16 +3,33 @@
 
 (defunits-of weight :kilogram
   "WiP"
-  :kg :kilogram
+  :kg #{:kilograms :kilogram}
   :g 1/1000
   :gram #{:grams :g}
   
   :hg [100 :grams]
-  :hg :hectogram
+  :hg #{:hectograms :hectogram}
 
   :dg [1/10 :g]
-  :dg :decigram
+  :dg #{:decigrams :decigram}
   
   :dag [10 :g]
-  :dag :decagram
-)
+  :dag #{:decagrams :decagram}
+  
+  :cg [1/10 :dg]
+  :mg [1/10 :cg]
+  :Mg 1000
+  :Mg #{:tonne :metric-ton}
+  :Gg [1000 :Mg]
+  :Tg [1000 :Gg]
+  :Pg [1000 :Tg]
+  :Eg [1000 :Pg]
+  :Zg [1000 :Eg]
+  :Yg [1000 :Zg]
+  :mcg [1/1000 :g]
+  :ng [1/1000 :mcg]
+  :pg [1/1000 :ng]
+  :fg [1/1000 :pg]
+  :ag [1/1000 :fg]
+  :zg [1/1000 :ag]
+  :yg [1/1000 :yg])
