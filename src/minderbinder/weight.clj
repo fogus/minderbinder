487b0219be2abc0735927f9e3d851a08d8f3fb22
fogus <mefogus@gmail.com>
2013-06-04 08:23:49 -0400

    Starting on a first pass at weights. mg

--- src/minderbinder/weight.clj
+++ src/minderbinder/weight.clj
@@ -6,6 +6,9 @@
   :kg #{:kilograms :kilogram}
   :g 1/1000
   :gram #{:grams :g}
+
+  :mg [1/1000 :g]
+  :mg #{:milligrams :milligram}
   
   :hg [100 :grams]
   :hg #{:hectograms :hectogram}
@@ -15,21 +18,4 @@
   
   :dag [10 :g]
   :dag #{:decagrams :decagram}
-  
-  :cg [1/10 :dg]
-  :mg [1/10 :cg]
-  :Mg 1000
-  :Mg #{:tonne :metric-ton}
-  :Gg [1000 :Mg]
-  :Tg [1000 :Gg]
-  :Pg [1000 :Tg]
-  :Eg [1000 :Pg]
-  :Zg [1000 :Eg]
-  :Yg [1000 :Zg]
-  :mcg [1/1000 :g]
-  :ng [1/1000 :mcg]
-  :pg [1/1000 :ng]
-  :fg [1/1000 :pg]
-  :ag [1/1000 :fg]
-  :zg [1/1000 :ag]
-  :yg [1/1000 :yg])
+)
