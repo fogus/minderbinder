4388d5cbfb326a51f80ff34bfe6d7a12ff4b3f6b
fogus <mefogus@gmail.com>
2013-06-04 08:51:40 -0400

    Added temperature ns using scale and offset

--- src/minderbinder/temperature.clj
+++ src/minderbinder/temperature.clj
@@ -1,3 +1,14 @@
 (ns minderbinder.temperature
   (:require [minderbinder.core :refer (defunits-of)]))
 
+(defunits-of temperature :kelvin
+  "TODO"
+  :celcius    {:scale 1 :offset 273.15}
+  :fahrenheit {:scale 5/9 :offset 255.37})
+
+
+(comment
+
+
+
+)
