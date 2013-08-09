fd2f1ca6e8ce4d69b2b422d9b637e0e8b8a6b351
Fogus <mefogus@gmail.com>
2013-08-09 09:03:04 -0400

    Added angular measure

---
+++ src/minderbinder/angular.clj
@@ -0,0 +1,10 @@
+(ns minderbinder.angular
+  (:require [minderbinder.core :refer (defunits-of)]))
+
+(defunits-of angular :radian
+  ""
+
+  ;; The angle subtended at the center of a circle by
+  ;; an arc equal in length to the radius of the
+  ;; circle.
+  :radian #{:rad})
