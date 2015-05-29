0bd403743459eec4d17aee4652ddab2335824dc9
fogus <mefogus@gmail.com>
2015-05-28 21:04:33 -0400

    Latest Clojure 1.6.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject fogus/minderbinder "0.3.0-SNAPSHOT"
   :description "Converting one thing into another thing via Clojure at read time and compile time."
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
   :plugins [[marginalia "0.8.0-SNAPSHOT"]])
