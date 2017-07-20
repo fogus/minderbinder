c01ca3a791ef7e88be190ac884c50de2f60c4c71
fogus <mefogus@gmail.com>
2017-07-20 09:31:27 -0400

    Upped ver. Moved units to uom ns.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject fogus/minderbinder "0.3.0-SNAPSHOT"
-  :description "Converting one thing into another thing via Clojure at read time and compile time."
-  :dependencies [[org.clojure/clojure "1.8.0-master-SNAPSHOT"]]
+(defproject fogus/minderbinder "0.4.0-SNAPSHOT"
+  :description "Converting one thing into another thing via Clojure at run, read, and compile time."
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]]
   :plugins [[marginalia "0.8.0-SNAPSHOT"]]
   :repositories [["snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"]])
