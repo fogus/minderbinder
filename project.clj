35b10b279b241fbeab38f63af568e6302f40930a
fogus <mefogus@gmail.com>
2017-09-07 11:22:03 -0400

    bumped to v0.3.0 to handle ns issues highlighted by Spec.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject fogus/minderbinder "0.3.0-SNAPSHOT"
+(defproject fogus/minderbinder "0.3.0"
   :description "Converting one thing into another thing via Clojure at read time and compile time."
-  :dependencies [[org.clojure/clojure "1.8.0-master-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :plugins [[marginalia "0.8.0-SNAPSHOT"]]
-  :repositories [["snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"]])
+  :repositories [["snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"]]
+  :deploy-repositories [["clojars" {:sign-releases false}]])
